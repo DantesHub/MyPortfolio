fbbd6561a0f78474101c76f50e079c054b8233fb
DantesHub <51376601+DantesHub@users.noreply.github.com>
2020-09-10 03:05:45 +0000

    Deploying to gh-pages from  @ 5d6daaa3d34d07261eded01fb56865148fe511b8 🚀

---
+++ precache-manifest.40c0cf43600af3a446dde8c63d7b2d92.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0963ccf19365f166e3203d310b43086a",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "de7a8fb600e1896b7cba",
+    "url": "/myportfolio/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "3ce1f88e7ba199bc1ef0",
+    "url": "/myportfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "7dcf072acfc793fc13cc",
+    "url": "/myportfolio/static/css/main.d6d49ebc.chunk.css"
+  },
+  {
+    "revision": "fe4e95000846ccdaa2bc",
+    "url": "/myportfolio/static/js/2.a5d26cc5.chunk.js"
+  },
+  {
+    "revision": "de7a8fb600e1896b7cba",
+    "url": "/myportfolio/static/js/3.1173e29f.chunk.js"
+  },
+  {
+    "revision": "3ce1f88e7ba199bc1ef0",
+    "url": "/myportfolio/static/js/4.38063070.chunk.js"
+  },
+  {
+    "revision": "7dcf072acfc793fc13cc",
+    "url": "/myportfolio/static/js/main.2d5d348f.chunk.js"
+  },
+  {
+    "revision": "cf0c28b1c8962310b2b5",
+    "url": "/myportfolio/static/js/runtime-main.2c13bd37.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/myportfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/myportfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ea70cde9f37d18f2d1ace6e8b16d1e64",
+    "url": "/myportfolio/static/media/V.ea70cde9.png"
+  },
+  {
+    "revision": "b9a9794ea3a166ce5225d7efec322cb8",
+    "url": "/myportfolio/static/media/alarm2.b9a9794e.png"
+  },
+  {
+    "revision": "ac3eac0b0af82236aac6f39af176d266",
+    "url": "/myportfolio/static/media/breastcancer.ac3eac0b.png"
+  },
+  {
+    "revision": "7af20511aba227f2a4b0bcd1c10275ab",
+    "url": "/myportfolio/static/media/bstack.7af20511.png"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/myportfolio/static/media/coding.46d9f22b.svg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/myportfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/myportfolio/static/media/deeplearning.7a9b2eb0.png"
+  },
+  {
+    "revision": "2784c022071ef04e6746d3b599e0da2b",
+    "url": "/myportfolio/static/media/devbook.2784c022.png"
+  },
+  {
+    "revision": "02e1f8ad788d01ce458081168cee96b9",
+    "url": "/myportfolio/static/media/elogo4.02e1f8ad.png"
+  },
+  {
+    "revision": "9664050207bf00d574c3153f6177e0a8",
+    "url": "/myportfolio/static/media/freelancer.96640502.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myportfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "237715051068568ccaefec21c65d2bfa",
+    "url": "/myportfolio/static/media/pfv.23771505.png"
+  },
+  {
+    "revision": "c6a6391ce79f90acc55d095be79bc514",
+    "url": "/myportfolio/static/media/pokedex.c6a6391c.png"
+  },
+  {
+    "revision": "d840e844000a4c8c9060d1b09610592f",
+    "url": "/myportfolio/static/media/pomo.d840e844.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/myportfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "eea26a1abda32b8c5e37c4e75a53d7d8",
+    "url": "/myportfolio/static/media/stanford.eea26a1a.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/myportfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eeb831cce3414581edb26dafebfa79ce.js"
+  "/myportfolio/precache-manifest.40c0cf43600af3a446dde8c63d7b2d92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
